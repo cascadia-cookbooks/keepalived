e1dbe37614edf5b6b8b060d113d83ac8907c870a
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-02-27 15:48:32 -0800

    Tests for service

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,4 +5,8 @@ describe 'keepalived::default' do
     it { should be_installed }
   end
 
+  describe service('keepalived') do
+    it { should be_enabled }
+    it { should be_running }
+  end
 end
